e9bb9ddb9142c1f677a794edcf57cbc7abb63308
milomacphail <milomacphail@gmail.com>
2019-10-18 21:00:59 -0400

    Database and routing partially up.

---
+++ server/routes/exercises.js
@@ -0,0 +1,11 @@
+const router = require ('express');
+let User = require('../models/exercise.model');
+
+
+
+
+
+
+
+
+module.exports = router;
---
+++ server/routes/users.js
@@ -0,0 +1,8 @@
+const router = require ('express');
+let User = require('../models/user.model');
+
+router.route
+
+
+
+module.exports = router;
