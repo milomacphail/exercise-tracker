1150c3d31b43a19dedb5ccc0418f80fb49ba14de
milomacphail <milomacphail@gmail.com>
2019-10-19 14:31:25 -0400

    Database connected, tested and functioning

--- server/server.js
+++ server/server.js
@@ -4,19 +4,19 @@ const mongoose = require('mongoose');
 
 require ('dotenv').config();
 
-const app =  express.Router();
+const app =  express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
-    );
-    const connection = mongoose.connection;
-    connection.once('open', () => {
-        console.log("Database open.");
-    })
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true , useUnifiedTopology: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
 
 
 const exercisesRouter = require('./routes/exercises');
@@ -28,5 +28,3 @@ app.use('/users', usersRouter);
 app.listen(port, ()  =>{
     console.log('Server is running on port ${port}`.');
 });
-
-module.exports = app;
